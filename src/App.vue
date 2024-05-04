fcc88e5acd8592a4960c17308a4b472f11cbb285
PAUL\alsrl <ur_developer92@naver.com>
2024-05-04 23:01:21 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,92 @@
+<template>
+  <div id="app">
+    <div>
+      <TodoHeader></TodoHeader>
+      <TodoInput v-on:addTodoItem="addOneItem"></TodoInput>
+      <TodoList v-bind:propsdata="todoItems"
+                v-on:removeItem="removeOneItem"
+                v-on:toggleItem="toggleOneItem">
+      </TodoList>
+      <TodoFooter v-on:clearAll="clearAllItems"></TodoFooter>
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader.vue'
+import TodoInput from './components/TodoInput.vue'
+import TodoList from './components/TodoList.vue'
+import TodoFooter from './components/TodoFooter.vue'
+
+export default {
+
+  data : function() {
+    return {
+      todoItems: []
+    }
+  },
+
+  // 컨테이너 역할
+  methods: {
+    addOneItem: function(todoItem) {
+      const obj = {completed: false, item: todoItem};
+      localStorage.setItem(todoItem, JSON.stringify(obj));
+      this.todoItems.push(obj);
+    },
+ 
+    removeOneItem : function(todoItem, index){
+      localStorage.removeItem(todoItem.item);
+      this.todoItems.splice(index, 1);
+    },
+
+    toggleOneItem : function(todoItem, index){
+      this.todoItems[index].completed = !this.todoItems[index].completed;
+      localStorage.removeItem(todoItem.item);
+      localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
+    },
+
+    clearAllItems : function() {
+      localStorage.clear();
+      this.todoItems = [];
+    }
+  },
+
+  created : function() {
+    if (localStorage.length > 0) {
+      for (let i = 0; i < localStorage.length; i ++){
+        if(localStorage.key(i) !== 'loglevel:webpack-dev-server'){
+          this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+        }
+      }
+    }
+  },
+
+  components : {
+    // 컴포넌트 태그명 : 컴포넌트 내용
+    'TodoHeader' : TodoHeader,
+    'TodoInput' : TodoInput,
+    'TodoList' : TodoList,
+    'TodoFooter' : TodoFooter,
+
+  }
+}
+</script>
+
+<style>
+body {
+  text-align: center;
+  background-color: #f6f6f6;
+}
+
+input {
+  border-style: groove;
+  width : 200px;
+}
+
+button {
+  border-style: groove;
+}
+.shadow {
+  box-shadow: 5px, 10px 10px rgba(0, 0, 0, 0.83);
+}
+</style>
---
+++ src/components/TodoInput.vue
@@ -0,0 +1,89 @@
+<template>
+    <div class="inputBox shadow">
+        <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo">
+        <span class="addContainer" v-on:click="addTodo">
+            <i class="fas fa-plus addBtn"></i>
+        </span>
+
+        <Modal v-if="showModal" @close="showModal = false">
+       
+        <template v-slot:header>
+            경고!
+            <i class="closeModalBtn fa-solid fa-xmark" @click="showModal = false"></i>
+        </template>
+
+        <template v-slot:body>
+            아무것도 입력하지 않으셨습니다.
+        </template>
+      </Modal>
+    </div>
+</template>
+
+<script>
+import Modal from './common/Modal.vue'
+
+export default {
+    data: function() {
+        return {
+            newTodoItem : "",
+            showModal: false
+        }
+    },
+
+    methods : {
+        addTodo : function() {
+        // console.log(this.newTodoItem);
+        // 저장하는 로직
+        if (this.newTodoItem !== ''){
+            this.$emit('addTodoItem', this.newTodoItem);
+            this.clearInput();
+            } else {
+                this.showModal = !this.showModal;
+            }
+        },
+        
+        clearInput : function() {
+            this.newTodoItem = '';
+        }
+    },
+
+    components : {
+        Modal : Modal
+    }
+}
+</script>
+
+<style scoped>
+input:focus {
+    outline : none;
+} 
+
+.inputBox {
+    background: white;
+    height: 50px;
+    line-height: 50px;
+    border-radius: 5px;
+}
+
+.inputBox input {
+    border-style: none;
+    font-size: 0.9rem;
+}
+
+.addContainer {
+    float: right;
+    background: linear-gradient(to right, #6478fb, #8763fb);
+    display: block;
+    width: 3rem;
+    border-radius: 0 5px 5px 0;
+}
+
+.addBtn {
+    color : white;
+    vertical-align: middle;
+}
+
+.closeModalBtn {
+    color : #42b983;
+}
+</style>
